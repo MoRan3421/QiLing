4c071577fc6d9093c45dfc3af465d8999db1abae
MoRan3421 <moran@qiling.ai>
2026-06-11 02:16:29 +0800

    remove-test-files

--- check_path.mjs
+++
@@ -1,4 +0,0 @@
-console.log('import.meta.url:', import.meta.url);
-console.log('process.argv[1]:', process.argv[1]);
-console.log('file://' + process.argv[1].replace(/\\/g, '/'));
-console.log('match:', import.meta.url === `file://${process.argv[1].replace(/\\/g, '/')}`);
--- test_browser.mjs
+++
@@ -1,4 +0,0 @@
-import('./server/brain/tools/BrowserAutomation.js').then(m => {
-  const b = new m.BrowserAutomation();
-  return b.initialize().then(() => console.log('OK')).catch(e => console.error('FAIL', e.message));
-});
